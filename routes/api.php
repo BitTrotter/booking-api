911d526bb0b7dc948bdce6f911e6bebab2facf4b
Angel Hernandez <indigital87@gmail.com>
2026-04-30 09:39:54 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -28,60 +28,52 @@ Route::group([
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
     Route::post('/me', [AuthController::class, 'me'])->name('me');
 });
-Route::group([
-    'middleware' => ['api', 'auth:api'],
-], function ($router) {
-    Route::get('/permissions', [PermissionController::class, 'index'])->middleware('permission:list_role');
-    Route::post('/permissions', [PermissionController::class, 'store'])->middleware('permission:create_role');
-    Route::delete('/permissions/{id}', [PermissionController::class, 'destroy'])->middleware('permission:delete_role');
+Route::middleware(['api', 'auth:api'])->group(function () {
+    Route::get('/permissions',          [PermissionController::class, 'index'])->middleware('permission:list_role,api');
+    Route::post('/permissions',         [PermissionController::class, 'store'])->middleware('permission:create_role,api');
+    Route::delete('/permissions/{id}',  [PermissionController::class, 'destroy'])->middleware('permission:delete_role,api');
 
-    Route::resource('role', RoleController::class)->middleware([
-        'index' => 'permission:list_role',
-        'show' => 'permission:list_role',
-        'store' => 'permission:create_role',
-        'update' => 'permission:edit_role',
-        'destroy' => 'permission:delete_role',
-    ]);
+    Route::get('/role',          [RoleController::class, 'index'])->middleware('permission:list_role,api');
+    Route::post('/role',         [RoleController::class, 'store'])->middleware('permission:create_role,api');
+    Route::get('/role/{role}',   [RoleController::class, 'show'])->middleware('permission:list_role,api');
+    Route::put('/role/{role}',   [RoleController::class, 'update'])->middleware('permission:edit_role,api');
+    Route::delete('/role/{role}',[RoleController::class, 'destroy'])->middleware('permission:delete_role,api');
 });
 Route::middleware(['auth:api'])->group(function () {
 
-    Route::apiResource('cabins', CabinController::class)->middleware([
-        'index' => 'permission:list_cabin',
-        'show' => 'permission:show_cabin_details',
-        'store' => 'permission:create_cabin',
-        'update' => 'permission:edit_cabin',
-        'destroy' => 'permission:delete_cabin',
-    ]);
-    Route::put('/cabins/{id}', CabinController::class . '@update')->middleware('permission:edit_cabin');
-    Route::post('/cabins/{id}/features', [CabinController::class, 'assignFeatures'])->middleware('permission:edit_cabin');
-    Route::post('/cabins/{cabin}/price', [CabinPriceController::class, 'calculate'])->middleware('permission:show_cabin_details');
-    Route::get('/cabins/{cabin}/price-rules', [CabinPriceRuleController::class, 'index'])->middleware('permission:show_cabin_details');
-    Route::post('/cabins/{cabin}/price-rules', [CabinPriceRuleController::class, 'store'])->middleware('permission:edit_cabin');
-    // actualizar regla
-    Route::put('/price-rules/{priceRule}', [CabinPriceRuleController::class, 'update'])->middleware('permission:edit_cabin');
-    // eliminar regla
-    Route::delete('/price-rules/{priceRule}', [CabinPriceRuleController::class, 'destroy'])->middleware('permission:delete_cabin');
+    Route::get('/cabins',           [CabinController::class, 'index'])->middleware('permission:list_cabin,api');
+    Route::post('/cabins',          [CabinController::class, 'store'])->middleware('permission:create_cabin,api');
+    Route::get('/cabins/{cabin}',   [CabinController::class, 'show'])->middleware('permission:show_cabin_details,api');
+    Route::put('/cabins/{cabin}',   [CabinController::class, 'update'])->middleware('permission:edit_cabin,api');
+    Route::patch('/cabins/{cabin}', [CabinController::class, 'update'])->middleware('permission:edit_cabin,api');
+    Route::delete('/cabins/{cabin}',[CabinController::class, 'destroy'])->middleware('permission:delete_cabin,api');
+    Route::post('/cabins/{id}/features', [CabinController::class, 'assignFeatures'])->middleware('permission:edit_cabin,api');
+    Route::post('/cabins/{cabin}/price', [CabinPriceController::class, 'calculate'])->middleware('permission:show_cabin_details,api');
+    Route::get('/cabins/{cabin}/price-rules', [CabinPriceRuleController::class, 'index'])->middleware('permission:show_cabin_details,api');
+    Route::post('/cabins/{cabin}/price-rules', [CabinPriceRuleController::class, 'store'])->middleware('permission:edit_cabin,api');
+    Route::put('/price-rules/{priceRule}', [CabinPriceRuleController::class, 'update'])->middleware('permission:edit_cabin,api');
+    Route::delete('/price-rules/{priceRule}', [CabinPriceRuleController::class, 'destroy'])->middleware('permission:delete_cabin,api');
     Route::get('/reservations/availability', [ReservationController::class, 'checkAvailability']);
-    Route::apiResource('reservations', ReservationController::class)->middleware([
-        'index' => 'permission:list_reservation',
-        'show' => 'permission:show_reservation_details',
-        'store' => 'permission:create_reservation',
-        'update' => 'permission:edit_reservation',
-        'destroy' => 'permission:cancel_reservation',
-    ]);
+    Route::get('/reservations',                  [ReservationController::class, 'index'])->middleware('permission:list_reservation,api');
+    Route::post('/reservations',                 [ReservationController::class, 'store'])->middleware('permission:create_reservation,api');
+    Route::get('/reservations/{reservation}',    [ReservationController::class, 'show'])->middleware('permission:show_reservation_details,api');
+    Route::put('/reservations/{reservation}',    [ReservationController::class, 'update'])->middleware('permission:edit_reservation,api');
+    Route::patch('/reservations/{reservation}',  [ReservationController::class, 'update'])->middleware('permission:edit_reservation,api');
+    Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy'])->middleware('permission:cancel_reservation,api');
+
     Route::apiResource('features', FeatureController::class);
-    Route::apiResource('users', UsersController::class)->middleware([
-        'index' => 'permission:list_staff',
-        'show' => 'permission:list_staff',
-        'store' => 'permission:create_staff',
-        'update' => 'permission:edit_staff',
-        'destroy' => 'permission:delete_staff',
-    ]);
+
+    Route::get('/users',           [UsersController::class, 'index'])->middleware('permission:list_staff,api');
+    Route::post('/users',          [UsersController::class, 'store'])->middleware('permission:create_staff,api');
+    Route::get('/users/{user}',    [UsersController::class, 'show'])->middleware('permission:list_staff,api');
+    Route::put('/users/{user}',    [UsersController::class, 'update'])->middleware('permission:edit_staff,api');
+    Route::patch('/users/{user}',  [UsersController::class, 'update'])->middleware('permission:edit_staff,api');
+    Route::delete('/users/{user}', [UsersController::class, 'destroy'])->middleware('permission:delete_staff,api');
 
     // Cabin images
-    Route::get('/cabins/{id}/images', [CabinImageController::class, 'index'])->middleware('permission:show_cabin_details');
-    Route::post('/cabins/{id}/images', [CabinImageController::class, 'store'])->middleware('permission:edit_cabin');
-    Route::delete('/cabins/{id}/images/{imageId}', [CabinImageController::class, 'destroy'])->middleware('permission:delete_cabin');
+    Route::get('/cabins/{id}/images', [CabinImageController::class, 'index'])->middleware('permission:show_cabin_details,api');
+    Route::post('/cabins/{id}/images', [CabinImageController::class, 'store'])->middleware('permission:edit_cabin,api');
+    Route::delete('/cabins/{id}/images/{imageId}', [CabinImageController::class, 'destroy'])->middleware('permission:delete_cabin,api');
 
     // Payments
     Route::get('/payments', [PaymentController::class, 'index']);
