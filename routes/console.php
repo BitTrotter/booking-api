5787d2dfd64b0f208d7e58a78d0fef01b343c919
Angel Hernandez <indigital87@gmail.com>
2026-05-25 20:26:19 -0600

    test schedule

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('reservations:activate')->dailyAt('00:05');
+Schedule::command('reservations:activate')->dailyAt('20:55');
